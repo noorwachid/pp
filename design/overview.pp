fc53c8e73dd72dffe6761a28501a8375fe58367d
Noor Wachid <wachid@fisip.net>
2023-08-13 23:48:32 +0700

    update design

--- design/overview.pp
+++ design/overview.pp
@@ -1,10 +1,14 @@
+# TYPE INSTANCIATION
 # typed
 # cpp: int typed = 10
 var type: Int = 10
 
 # deduced
 # cpp: auto deduced = 10
-var deduced := 10
+var deduced = 10
+
+# constant
+const pi: Float = 3
 
 # primitive
 var bool: Bool
@@ -27,6 +31,11 @@ pointerOfInt = &int
 # underlying data pointed
 print(*pointerOfInt)
 
+pointerOfAnimal = new Animal()
+# syntactic sugar for (*pointerOfInt).speak()
+pointerOfAnimal:speak()
+
+
 # reference
 # cpp: int& referenceOfInt = int
 refer var referenceOfInt: Int = int
@@ -36,8 +45,15 @@ refer var referenceOfInt: Int = int
 # cpp: int&& movedValueOfInt = static_cast<int&&>(int)
 move var movedValueOfInt: Int = int 
 
+# BUILT-IN TYPE
+# optional
+var optionalInt: Optional<Int>
+
 # tuple
 var tuple: Tuple<Bool, Int, String>
+tuple.0 = true
+tuple.1 = 123
+tuple.3 = "whoa"
 
 # resizable string
 var string: String
@@ -46,7 +62,7 @@ var string: String
 var fixedString: FixedString<11> = "hello world"
 
 # non owning sub-string
-var mirageString := MirageString(fixedString.data, fixedString.length)
+var mirageString = MirageString(fixedString.data, fixedString.length)
 
 # resizeable array
 var array: Array<Int> = { 1, 2, 3 }
@@ -57,17 +73,20 @@ array.add(5)
 var fixedArray: FixedArray<Int, 3> = { 1, 2, 3 }
 
 # non owning sub-array
-var mirageArray := MirageArray<Int, 2>(fixedArray.data + 1, 2)
+var mirageArray = MirageArray<Int, 2>(fixedArray.data + 1, 2)
 
-# dictionary
+# key, value type with hash table
 var dictionary: Dictionary<String, String> = {
 	"int" = "",
 }
 
-# dictionary with Array<Tuple<> as underlying memory layout
-var arrayDictionary: ArrayDictionary<String, String> = {
+# key, value type without hash table
+# Array<Tuple<Key, Value>> as underlying memory layout 
+var thesaurus: Thesaurus<String, String> = {
 	"int" = "",
 }
+
+# USER DEFINED TYPE
 
 # enum is always scoped
 enum State {
@@ -77,34 +96,54 @@ enum State {
 	rejected
 }
 
-# struct cannot hold method, constructor and destructor
-struct WindowData {
-	var title: String
+# namespace 
+namespace OS {
+	# struct cannot hold method, constructor and destructor
+	struct WindowData {
+		var title: String
+	}
 }
 
-class Person {
+# bleed the namespace to the current file only
+use OS
+
+class Enemy {
 	# by convention leading underscore is private
     # namespace access :: is replaced by .
+	var _id: ECS.ID
 	var _name: String
-	var _age: int
+	var _age: Int
+	var _health: Int
+	var _damage: Int
 
-	# will generate std::string () const
-    func toString() -> String {
-		return "Person: {0} {1}".format(_name, _age)
+    func restore() {
+		_health = 100
     }
+
+	# overridable by derived class
+	virtualize func attack(refer var player: Player) {
+		player.health -= _damage
+	}
 
 	# add set keyword to allow mutation 
 	# and add syntaxtic sugar for person.name = "new name"
-	# will generate void name(const std::string& name)
-	set func name(value: &String) {
+	set func name(value: String) {
 		_name = value
 	}
 
 	# add get keyword to allow mutation 
 	# and add syntaxtic sugar for const name = person.name
-	# will generate const std::string& name() const
-	get func name() -> &String {
+	get func name() -> String {
 		return _name
+	}
+}
+
+# derived class
+class WizardHead: Enemy {
+	# override virtual function
+	override func attack(refer var player: Player) {
+		player.health -= damage
+		player.mana -= damage / 2
 	}
 }
 
@@ -122,6 +161,60 @@ class Vector {
 	}
 }
 
+# USER DEFINED FUNCTION
+
 func main(arguments: Array<String>) -> int {
-	return 0;
+	return 0
 }
+
+func affect(refer var int: Int) {
+	int++
+}
+
+# CONTROL FLOW
+
+if true || false {
+	print("it's true")
+}
+
+if true && false && !false {
+	print("it's not happening")
+} else {
+	print("it's happening")
+}
+
+var number = 10
+
+switch number {
+	case 1 {
+		print("one")
+	}
+	case 2 {
+		print("two")
+	}
+	case 3 || 4 {
+		print("three or four")
+	}
+	default {
+		print("unknown")
+	}
+}
+
+# LOOP
+# good-old for loop
+for var i = 0; i < 10; i++ {
+	print(i)
+}
+
+# range-based loop
+for i in 1...10 {
+	print(i)
+}
+
+# while loop
+for true {
+}
+
+# no do {} while() who needed it anyway
+
+
--- vim/syntax/pp.vim
+++ vim/syntax/pp.vim
@@ -1,5 +1,5 @@
 " Vim syntax file
-" Language:     C Squared
+" Language:     ++
 " Author:       Noor Wachid
 " URL:          https://github.com/noorwachid/vim-gularen/
 
@@ -8,24 +8,26 @@ if exists("b:current_syntax")
 endif
 
 " Inlines
-syn match csqComment "# .*$" 
-syn keyword csqKeyword const var refer move
-syn keyword csqKeyword func return null
-syn keyword csqKeyword class struct operate mutate get set
-syn keyword csqKeyword enum 
+syn match ppComment "# .*$" 
+syn keyword ppKeyword const var refer move as
+syn keyword ppKeyword func return
+syn keyword ppKeyword namespace use class struct operate mutate virtualize override get set
+syn keyword ppKeyword enum 
+syn keyword ppKeyword false true null
+syn keyword ppKeyword if else switch case default for in
 
-syn match csqType "\<[A-Z][A-Za-z0-9_]*\>" 
-syn match csqFunction "\<[a-z_][A-Za-z0-9_]*("me=e-1
-syn match csqFunction "\<[a-z_][A-Za-z0-9_]*<"me=e-1
+syn match ppType "\<[A-Z][A-Za-z0-9_]*\>" 
+syn match ppFunction "\<[a-z_][A-Za-z0-9_]*("me=e-1
+syn match ppFunction "\<[a-z_][A-Za-z0-9_]*<"me=e-1
 
-syn match csqNumber "\<[0-9\.]\+\>"
-syn region csqString start='"' end='"'
+syn match ppNumber "\<[0-9\.]\+\>"
+syn region ppString start='"' end='"'
 
-hi def link csqComment Comment
-hi def link csqKeyword Keyword
-hi def link csqType Type
-hi def link csqFunction Function
-hi def link csqString String
-hi def link csqNumber Number
+hi def link ppComment Comment
+hi def link ppKeyword Keyword
+hi def link ppType Type
+hi def link ppFunction Function
+hi def link ppString String
+hi def link ppNumber Number
 
-let b:current_syntax = "csq"
+let b:current_syntax = "pp"
